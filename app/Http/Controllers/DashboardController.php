a3cf0692532af6cdd54d2fc637bbeb9157f69330
Atha Adnan <athaadnanreswara191@gmail.com>
2026-01-08 19:23:50 +0700

    fungsi login

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function login()
+    {
+        $user = Auth::user();
+
+        if ($user->role === 'admin') {
+            return view('admin.dashboard', compact('user'));
+        }elseif($user->role === 'pegawai'){
+            return view('pegawai.dashboard', compact('user'));
+        }else{
+            abort(403, 'Akses Ditolak. Anda tidak memiliki izin untuk membuka halaman ini .');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+//Routes Login dan Logout
+Route::middleware('guest')->group(function (){
+    Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
+
+//Prefix Admin
+Route::prefix('admin')->name('admin.')->middleware(['auth', 'role:admin'])->group(function (){
+    //Dashboard Admin
+    Route::get('dashboard', [DashboardController::class, 'login'])->name('dashboard');
+
 });
